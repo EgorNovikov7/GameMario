23ba41fc28d0dbc44ea9326926bcb0dd74aab138
Novikov Egor <your_email@whatever.com>
2024-09-22 11:47:21 +0700

    Добавил основные типы объекта

--- GameMario/GameMario.cpp
+++ GameMario/GameMario.cpp
@@ -1,20 +1,74 @@
-﻿// GameMario.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+ 
 
-#include <iostream>
+struct Mario {
+    float x;
+    float y;
+    int width;
+    int weight;
+    int lives;
+    int state;
+    int isJump;
+};
+
+struct Platform {
+    float x;
+    float y;
+    int with;
+    int height;
+    int type;
+
+};
+
+struct Enemy {
+    float x;
+    float y;
+    int width;
+    int weight;
+    int state;
+};
+
+struct Coin{
+    float x;
+    float y;
+    int collected;
+};
+
+struct Star
+{
+    float x;
+    float y;
+    int collected;
+};
+
+struct Key
+{
+    float x;
+    float y;
+
+};
+
+struct Door {
+    float x;
+    float y;
+    int width;
+    int height;
+    int locked;
+};
+
+struct Map{
+    int x, y;
+    float width;
+    float weight;
+    float scrollSpeed;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "Rus");
+    printf("\n");
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+ 
